93f9172c096e0635f2d1c927e71540d68efb9903
rfpoulos <rfpoulos@outlook.com>
2018-03-21 11:44:32 -0400

    refactor es2017

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //Some code for review
-var http = require('http');
+const http = require('http');
 const fs = require('fs');
 const readline = require('readline');
 const promisify = require('util').promisify;
@@ -9,29 +9,29 @@ const rl = readline.createInterface({
     output: process.stdout
   });
 
-var readlineAsPromise = promisify(fs.readFile);
-var writeFileAsPromise = promisify(fs.writeFile);
+const readlineAsPromise = promisify(fs.readFile);
+const writeFileAsPromise = promisify(fs.writeFile);
 
-var saveContacts = function(contacts) {
+let saveContacts = function(contacts) {
     writeFileAsPromise('phoneBook.txt', JSON.stringify(contacts));
 }
 
-var findContact = function(id, contacts) {
+let findContact = function(id, contacts) {
     id = parseInt(id, 10);
     return contacts.find(function(contact) {
         return contact.id === id;
     });
 };
 
-var deleteContact = function(contactToDelete, contacts) {
-    var newContacts = contacts.filter(function(contact) {
+let deleteContact = function(contactToDelete, contacts) {
+    let newContacts = contacts.filter(function(contact) {
         return contact !== contactToDelete;
     });
     saveContacts(newContacts);
 };
 
-var readBody = function(request, callback) {
-    var body = '';
+let readBody = function(request, callback) {
+    let body = '';
     request.on('data', function(chunk) {
         body += chunk.toString();
     });
@@ -40,23 +40,21 @@ var readBody = function(request, callback) {
     });
 };
 
-var matches = function(request, method, path) {
+let matches = function(request, method, path) {
     return request.method === method &&
            request.url.startsWith(path);
 };
 
-var getSuffix = function(fullUrl, prefix) {
-    return fullUrl.slice(prefix.length);
-};
+let getSuffix = (fullUrl, prefix) => fullUrl.slice(prefix.length);
 
-var getContacts = function(request, response, contacts) {
+let getContacts = function(request, response, contacts) {
     response.end(JSON.stringify(contacts));
 };
 
-var postContacts = function(request, response, contacts) {
+let postContacts = function(request, response, contacts) {
     readBody(request, function(body) {
-        var contact = JSON.parse(body);
-        var lastId = contacts[contacts.length - 1]['id'];
+        let contact = JSON.parse(body);
+        let lastId = contacts[contacts.length - 1]['id'];
         contact.id = ++lastId;
         console.log(contact);
         contacts.push(contact);
@@ -65,38 +63,38 @@ var postContacts = function(request, response, contacts) {
     });
 };
 
-var deleteContactfromContacts = function(request, response, contacts) {
-    var id = getSuffix(request.url, '/contacts/');
-    var contact = findContact(id, contacts);
+let deleteContactfromContacts = function(request, response, contacts) {
+    let id = getSuffix(request.url, '/contacts/');
+    let contact = findContact(id, contacts);
     deleteContact(contact, contacts);
     console.log(contact);
     response.end('Deleted contact!');
 };
 
-var getContact = function(request, response, contacts) {
-    var id = getSuffix(request.url, '/contacts/');
-    var contact = findContact(id, contacts);
+let getContact = function(request, response, contacts) {
+    let id = getSuffix(request.url, '/contacts/');
+    let contact = findContact(id, contacts);
     response.end(JSON.stringify(contact));
 };
 
-var putContact = function(request, response, contacts) {
-    var id = getSuffix(request.url, '/contacts/');
-    var contact = findContact(id, contacts);
+let putContact = function(request, response, contacts) {
+    let id = getSuffix(request.url, '/contacts/');
+    let contact = findContact(id, contacts);
     readBody(request, function(body) {
-        var newParams = JSON.parse(body);
+        let newParams = JSON.parse(body);
         Object.assign(contact, newParams);
         saveContacts(contacts);        
         response.end('Updated contact!');
     });
 };
 
-var notFound = function(request, response) {
+let notFound = function(request, response) {
     response.statusCode = 404;
     response.end('404, nothing here!');
 };
 
 
-var routes = [
+let routes = [
     { method: 'DELETE', path: '/contacts/', handler: deleteContactfromContacts },
     { method: 'GET', path: '/contacts/', handler: getContact },
     { method: 'PUT', path: '/contacts/', handler: putContact },
@@ -104,12 +102,10 @@ var routes = [
     { method: 'POST', path: '/contacts', handler: postContacts },
 ];
 
-var server = http.createServer(function(request, response) {
+let server = http.createServer(function(request, response) {
     readlineAsPromise('phoneBook.txt').then(function(data){
-        var contacts = JSON.parse(data);
-        var route = routes.find(function(route) {    
-            return matches(request, route.method, route.path);
-        });
+        let contacts = JSON.parse(data);
+        let route = routes.find(route => matches(request, route.method, route.path));
     
         (route ? route.handler : notFound)(request, response, contacts);
     })
